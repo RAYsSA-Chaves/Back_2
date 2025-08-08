56eaa87d1f9bbed421e719c6c7944c7b1566ce5e
unknown <rayssa.ccmelo@gmail.com>
2025-08-08 16:38:30 -0300

    crud completo

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('autores', AutoresView.as_view())
+    path('autores', AutoresView.as_view()),
+    path('crud/<int:pk>', AutoresCrud.as_view())
 ]
 
 # urlpatterns = paleta de end points
--- api/views.py
+++ api/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Autor
 from .serializers import AutorSerializers
 
 class AutoresView(ListCreateAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializers
+
+class AutoresCrud(RetrieveUpdateDestroyAPIView):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializers

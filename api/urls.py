ab6788868b683d153d61d837e6550600a9245d81
unknown <rayssa.ccmelo@gmail.com>
2025-08-14 16:54:53 -0300

    bla

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('autores', AutoresView.as_view()),
-    path('crud/<int:pk>', AutoresCrud.as_view())
+    path('crud/<int:pk>', AutoresCrud.as_view()),
+    path('authors', listar_autores)
 ]
 
 # urlpatterns = paleta de end points

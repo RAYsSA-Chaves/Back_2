bf78efe474aae9347f4b597c311ada2995da9d3f
RAYsSA-Chaves <rayssa.ccmelo@gmail.com>
2025-10-02 16:57:00 -0300

    Login

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { HomeComponent } from './pages/home/home.component';
 import { AutoresPage } from './pages/authors/authors.component';
 import { EditorasPage } from './pages/editoras/editoras.component';
 import { LivrosPage } from './pages/livros/livros.component';
+import { LoginComponent } from './pages/login.component/login.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', component: LoginComponent },
     { path: 'home', component: HomeComponent },
     { path: 'autores', component: AutoresPage },
     { path: 'editoras', component: EditorasPage },
